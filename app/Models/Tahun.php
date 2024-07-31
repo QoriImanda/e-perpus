c0adea6aaff51b82aa7eb64e343b91aa40b2c7cc
Qori Imanda <imandaqori@gmail.com>
2024-07-31 21:30:56 +0700

    update

--- app/Models/Tahun.php
+++ app/Models/Tahun.php
@@ -9,34 +9,66 @@ class Tahun
 {
     public static function GetTahun()
     {
+        // if(Auth::user()->is_admin == TRUE){
+        //     $Januari = Sirkulasi::whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Feburari = Sirkulasi::whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Maret = Sirkulasi::whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $April = Sirkulasi::whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Mei = Sirkulasi::whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Juni = Sirkulasi::whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Juli = Sirkulasi::whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Agustus = Sirkulasi::whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $September = Sirkulasi::whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Oktober = Sirkulasi::whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $November = Sirkulasi::whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Desember = Sirkulasi::whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+
+        //     return [$Januari, $Feburari, $Maret, $April, $Mei, $Juni, $Juli, $Agustus, $September, $Oktober, $November, $Desember];
+        // }else{
+        //     $Januari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Feburari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Maret = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $April = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Mei = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Juni = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Juli = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Agustus = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $September = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Oktober = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $November = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+        //     $Desember = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+
+        //     return [$Januari, $Feburari, $Maret, $April, $Mei, $Juni, $Juli, $Agustus, $September, $Oktober, $November, $Desember];
+        // }
+
         if(Auth::user()->is_admin == TRUE){
-            $Januari = Sirkulasi::whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Feburari = Sirkulasi::whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Maret = Sirkulasi::whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $April = Sirkulasi::whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Mei = Sirkulasi::whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Juni = Sirkulasi::whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Juli = Sirkulasi::whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Agustus = Sirkulasi::whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $September = Sirkulasi::whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Oktober = Sirkulasi::whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $November = Sirkulasi::whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Desember = Sirkulasi::whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+            $Januari = Sirkulasi::whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->count();
+            $Feburari = Sirkulasi::whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->count();
+            $Maret = Sirkulasi::whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->count();
+            $April = Sirkulasi::whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->count();
+            $Mei = Sirkulasi::whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->count();
+            $Juni = Sirkulasi::whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->count();
+            $Juli = Sirkulasi::whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->count();
+            $Agustus = Sirkulasi::whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->count();
+            $September = Sirkulasi::whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->count();
+            $Oktober = Sirkulasi::whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->count();
+            $November = Sirkulasi::whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->count();
+            $Desember = Sirkulasi::whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->count();
 
             return [$Januari, $Feburari, $Maret, $April, $Mei, $Juni, $Juli, $Agustus, $September, $Oktober, $November, $Desember];
         }else{
-            $Januari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Feburari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Maret = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $April = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Mei = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Juni = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Juli = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Agustus = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $September = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Oktober = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $November = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
-            $Desember = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->whereBetween('status', [Sirkulasi::STATUS_APPROVAL_ADMIN, Sirkulasi::STATUS_SETELAH_BAYAR_DENDAM])->count();
+            $Januari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '01')->whereYear('created_at', Carbon::now()->year)->count();
+            $Feburari = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '02')->whereYear('created_at', Carbon::now()->year)->count();
+            $Maret = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '03')->whereYear('created_at', Carbon::now()->year)->count();
+            $April = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '04')->whereYear('created_at', Carbon::now()->year)->count();
+            $Mei = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '05')->whereYear('created_at', Carbon::now()->year)->count();
+            $Juni = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '06')->whereYear('created_at', Carbon::now()->year)->count();
+            $Juli = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '07')->whereYear('created_at', Carbon::now()->year)->count();
+            $Agustus = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '08')->whereYear('created_at', Carbon::now()->year)->count();
+            $September = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '09')->whereYear('created_at', Carbon::now()->year)->count();
+            $Oktober = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '10')->whereYear('created_at', Carbon::now()->year)->count();
+            $November = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '11')->whereYear('created_at', Carbon::now()->year)->count();
+            $Desember = Sirkulasi::where('id_user', Auth::user()->id)->whereMonth('created_at', '12')->whereYear('created_at', Carbon::now()->year)->count();
 
             return [$Januari, $Feburari, $Maret, $April, $Mei, $Juni, $Juli, $Agustus, $September, $Oktober, $November, $Desember];
         }
